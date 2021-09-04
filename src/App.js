88304ff04f700f28abc46afcf27bfe92c5e86c1d
6urrp <sndrtasic@gmail.com>
2021-09-04 15:51:54 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import SingleUser from './Components/Users/SingleUser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
 }
 
