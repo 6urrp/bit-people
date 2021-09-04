e186b2a3290d8f7643cdfffe3a2b5b245bb5ab02
6urrp <sndrtasic@gmail.com>
2021-09-04 16:14:48 +0200

    bootstrap added to grid component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import SingleUser from './Components/Users/SingleUser';
+import SingleGridUser from './Components/Users/GridUsers/SingleGridUser';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+    return (
+        <div>
+        <SingleGridUser />
+        <SingleUser />
+        </div>
+    )
 }
 
 export default App;
